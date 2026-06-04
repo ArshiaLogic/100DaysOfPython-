4a59691928f02709c969fd3c34996aae107206b6
ArshiaDev-frontEnd <arshiakarimi624@gmail.com>
2026-06-04 23:17:45 +0330

    new learn

--- lear-php DAY 2/learn3.py
+++ lear-php DAY 2/learn3.py
@@ -1,4 +1,25 @@
 # print(8/3)
 # print(int(8 / 3))
 #we can use round function for rounding numbers to integer numbers
-print(round(8 / 3))
+# print(round(8 / 3))
+# print(round(9 / 11, 3))
+# print(8 // 3)
+# result = 4 / 2
+# result /= 2
+# print(result)
+
+score = 0 
+
+
+#User scores a point
+# score += 1
+# score -= 1
+# score *= 1
+# score /= 1
+# print(score)
+
+score = 0
+height = 1.8
+isWinning = True
+#f-String با اف می تونیم هر دیتا تایپی رو توی یه خط پرینت کنیم
+print(f"your score is {score}, your height is {height}, your winning is {isWinning} ")
