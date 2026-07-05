b8eaec7e3adddc70b405d81e19e2a1e6c058d400
ArshiaDev-frontEnd <arshiakarimi624@gmail.com>
2026-07-05 23:53:32 +0330

    new changes

--- learn-php DAY 5/project0.py
+++ learn-php DAY 5/project0.py
@@ -4,18 +4,87 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
+#------------------input------------------
 print("Welcome to the PyPassword Generator!")
-# nr_letters = int(input("How many letters would you like in your Password?\n"))
-# nr_symbols = int(input(f"How many symbols would you like?\n"))
-# nr_numbers = int(input(f"How many numbers would you like?\n"))
-nr_letters = 3
-nr_symbols = 4
-nr_numbers = 2
-#Hard Level
+nr_letters = int(input("How many letters would you like in your Password?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
-for i in range(0, nr_letters):
-  n = (random.randint(0, len(letters)))
-  ran_letter = letters[n]
+password = ""
+
+f
 
 
-print(ran_letter)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# #Hard Level
+# #------------------letter randomizer------------------
+# list_a = []
+# for i in range(0, nr_letters):
+#   n = (random.randint(0, len(letters) - 1))
+#   list_a.append(letters[n])
+
+# #------------------symbol randomizer------------------  
+# list_b = []
+# for i in range(0, nr_symbols):
+#   n = (random.randint(0, len(symbols) - 1))
+#   list_b.append(symbols[n])
+
+# #------------------number randomizer------------------  
+# list_c = []
+# for i in range(0, nr_numbers):
+#   n = (random.randint(0, len(numbers) - 1))
+#   list_c.append(numbers[n])
+
+
+# #------------------Random list------------------  
+# random_list = list_a + list_b + list_c
+
+# #------------------Random randomizer------------------  
+# new_random_list = []
+# for i in range(0, len(random_list)):
+#   n = (random.randint(0, len(random_list) - 1))
+#   new_random_list.append(random_list[n])
+
+# #------------------OUTPUT------------------  
+# print(("Here is your password: ") + ''.join(new_random_list))
+
+
+  
+
