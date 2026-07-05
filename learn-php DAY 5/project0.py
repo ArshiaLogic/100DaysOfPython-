5a536be74a357f937e67a7a35174bbf6fc14cf74
ArshiaDev-frontEnd <arshiakarimi624@gmail.com>
2026-07-05 18:49:01 +0330

    new

--- learn-php DAY 5/project0.py
+++ learn-php DAY 5/project0.py
@@ -1,4 +1,4 @@
-#Password Generator Project
+# #Password Generator Project
 import random
 letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
@@ -13,4 +13,9 @@ nr_symbols = 4
 nr_numbers = 2
 #Hard Level
 
-for i in range(0, 10):
+for i in range(0, nr_letters):
+  n = (random.randint(0, len(letters)))
+  ran_letter = letters[n]
+
+
+print(ran_letter)
